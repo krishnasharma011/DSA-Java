534554f5ff81fa6adfc1047d340f124130a9dfb8
krishnasharma011 <sharmakrishna20228@gmail.com>
2026-09-08 11:16:20 +0530

    Time: 700 ms (5.27%), Space: 46.8 MB (43.24%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -4,20 +4,12 @@ class Solution {
         List<Integer> li=new ArrayList<>();
         Arrays.sort(nums);
         permutations(ans,li,0,nums);
-        HashSet<List<Integer>> set=new HashSet<>();
-        for(int i=0;i<ans.size();i++){
-            set.add(ans.get(i));
-        }
-        List<List<Integer>> l=new ArrayList<>();
-        for(List<Integer> temp:set){
-            l.add(temp);
-        }
-        return l;
+        return ans;
     }
 
     private void permutations(List<List<Integer>> ans,List<Integer> curr,int idx,int[] nums){
         if(curr.size()==nums.length){
-            ans.add(new ArrayList<>(curr));
+            if(!ans.contains(curr)) ans.add(new ArrayList<>(curr));
             return ;
         }
         for(int i=0;i<curr.size()+1;i++){
