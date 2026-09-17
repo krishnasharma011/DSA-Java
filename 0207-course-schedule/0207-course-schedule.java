c48c7950e2dc89f2119f05ca1abdd49067263cac
krishnasharma011 <sharmakrishna20228@gmail.com>
2026-09-17 12:13:53 +0530

    Time: 4 ms (91.46%), Space: 46.7 MB (93.12%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -3,27 +3,34 @@ class Solution {
         List<List<Integer>> adj=new ArrayList<>();
         for(int i=0;i<numCourses;i++){
             adj.add(new ArrayList<>());
-        } 
-        int[] indegree=new int[numCourses];
+        }
         for(int i=0;i<prerequisites.length;i++){
-            int v=prerequisites[i][0];
-            int u=prerequisites[i][1];
-            adj.get(u).add(v);
-            indegree[v]++;
+            int u=prerequisites[i][0];
+            int v=prerequisites[i][1];
+            adj.get(v).add(u);
         }
-        Queue<Integer> q=new ArrayDeque<>();
-        List<Integer> ans=new ArrayList<>();
+        int[] state=new int[numCourses];
         for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0) q.add(i);
-        }
-        while(!q.isEmpty()){
-            int f=q.poll();
-            ans.add(f);
-            for(int ngr:adj.get(f)){
-                indegree[ngr]--;
-                if(indegree[ngr]==0) q.add(ngr);
+            if(state[i] == 0){
+                if(dfs(adj,i,state)){
+                    return false;
+                }
             }
         }
-        return ans.size()==numCourses;
-    }   
+        return true;
+    }
+
+    private boolean dfs(List<List<Integer>> adj,int s,int[] state){
+        state[s]=1;
+        for(int ngr:adj.get(s)){
+            if(state[ngr]==0){
+                if(dfs(adj,ngr,state)){
+                    return true;
+                }
+            }
+            else if(state[ngr]==1) return true;
+        }
+        state[s]=2;
+        return false;
+    }
 }
