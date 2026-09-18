84fe7a3451d337b0781e10c439132e1e36fb650d
krishnasharma011 <sharmakrishna20228@gmail.com>
2026-09-18 20:58:59 +0530

    Time: 0 ms (100%), Space: 41.6 MB (98.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,14 +1,11 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp=new int[n];
-        Arrays.fill(dp,-1);
-        return solve(n,0,dp);
-    }
-
-    private int solve(int n,int i,int[] dp){
-        if(i==n) return 1;
-        if(i>n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        return dp[i]=solve(n,i+1,dp)+solve(n,i+2,dp);
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
